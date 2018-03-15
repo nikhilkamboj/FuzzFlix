7bd1278fe0e615987408f05a045b9322c07a2644
Nikhil Kamboj <n.kamboj15@gmail.com>
2018-03-16 02:38:24 +0530

    app completed with all functionalities

--- app/src/main/java/com/example/nikhil/fuzzflix/fragments_main/FavouriteFragment.java
+++ app/src/main/java/com/example/nikhil/fuzzflix/fragments_main/FavouriteFragment.java
@@ -1,20 +1,63 @@
 package com.example.nikhil.fuzzflix.fragments_main;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.nikhil.fuzzflix.DetailPage;
+import com.example.nikhil.fuzzflix.MovieDataAdapter;
 import com.example.nikhil.fuzzflix.R;
+import com.example.nikhil.fuzzflix.constants.AppConstants;
+import com.example.nikhil.fuzzflix.database.Contract;
+import com.example.nikhil.fuzzflix.fragments_detail.FragmentViewPager;
 
 /**
  * Created by nikhil on 26/02/18.
  */
 
-public class FavouriteFragment extends Fragment {
+public class FavouriteFragment extends Fragment implements MovieDataAdapter.ListItemClickListener
+        , LoaderManager.LoaderCallbacks<Cursor>, FragmentViewPager {
+
+    private static final String[] projection = {
+            Contract.MainMoviesEntry.MOVIES_ID,
+            Contract.MainMoviesEntry.MOVIE_TITLE,
+            Contract.MainMoviesEntry.MOVIE_OVERVIEW,
+            Contract.MainMoviesEntry.MOVIE_FRONT_POSTER_PATH,
+            Contract.MainMoviesEntry.MOVIE_BACK_POSTER_PATH,
+            Contract.MainMoviesEntry.MOVIE_VOTE_AVERAGE,
+            Contract.MainMoviesEntry.MOVIE_VOTE_COUNT
+    };
+
+    private static final String TAG = FavouriteFragment.class.getSimpleName() ;
+
+
+    /**
+     * this for the type of loader, if there are different loading options
+     */
+    private static final int ID_FAVOURITE_LOADER = 41;
+
+    View mRootView;
+
+    RecyclerView mRecyclerView;
+
+    MovieDataAdapter mMovieAdapter;
+
+    // public ProgressBar mProgressBar;
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +66,107 @@ public class FavouriteFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.favourites_filter_fragment,container,false);
+        mRootView = inflater.inflate(R.layout.favourites_filter_fragment,container,false);
+
+        mRecyclerView = (RecyclerView) mRootView.findViewById(R.id.favourite_recycler_view);
+
+
+        mMovieAdapter = new MovieDataAdapter(this);
+
+        final GridLayoutManager layoutManager
+                = new GridLayoutManager(this.getActivity(), 2);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mRecyclerView.setAdapter(mMovieAdapter);
+
+//        //loadMovieData(AppConstants.getPopularFilterValue());
+//        getLoaderManager().initLoader(ID_FAVOURITE_LOADER,null,this);
+
+        return mRootView;
+    }
+
+    @Override
+    public void onResume() {
+        Log.i(TAG, "fav fragment has been resumed");
+        //loadMovieData(AppConstants.getPopularFilterValue());
+        getLoaderManager().initLoader(ID_FAVOURITE_LOADER,null,this);
+        super.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        Log.i(TAG, "fav fragment has started");
+        super.onStart();
+    }
+
+
+    @Override
+    public void onPause() {
+        Log.i(TAG, "fav fragment has been paused!!!");
+        getLoaderManager().destroyLoader(ID_FAVOURITE_LOADER);
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.i(TAG, "fav fragment has been stopped!!!");
+        super.onStop();
+    }
+
+    @Override
+    public void onListItemClick(int movie_id) {
+        Intent intent = new Intent(getContext(), DetailPage.class);
+
+        // sending the Mid would work as position might even change in due to data agregation of twp apis
+        intent.putExtra(AppConstants.getKeyMovieId(),movie_id);
+
+        startActivity(intent);
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case ID_FAVOURITE_LOADER:
+                Uri mPopularUri = Contract.MainMoviesEntry.CONTENT_URI;
+                String selection = Contract.MainMoviesEntry.MOVIE_IS_FAVOURITE+ "=?";
+                String[] selectionArgs = {"1"};
+
+                return new CursorLoader(getContext(),
+                        mPopularUri,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null);
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // cursor swapping taking place
+        Log.v(TAG,"cursor swapping taking place ");
+        mMovieAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+    /**
+     * interface methods to handle change of handling of loading of data in ViewPager
+     */
+    @Override
+    public void onPauseFragment() {
+        getLoaderManager().destroyLoader(ID_FAVOURITE_LOADER);
+    }
+
+    @Override
+    public void onResumeFragment() {
+        getLoaderManager().initLoader(ID_FAVOURITE_LOADER,null,this);
     }
 }
